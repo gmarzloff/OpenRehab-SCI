5ce2e5440f5a4ce2d03f075a7efde478468dcaac
George Marzloff <george@marzloffmedia.com>
2016-08-30 01:04:11 -0400

    mostly working

--- js/my-app.js
+++ js/my-app.js
@@ -1,14 +1,38 @@
-// Initialize your app
-var myApp = new Framework7();
+
+var isAndroid = Framework7.prototype.device.android === true;
+var isIos = Framework7.prototype.device.ios === true;
+
+// Set Template7 global devices flags
+Template7.global = {
+    android: isAndroid,
+    ios: isIos
+};
 
 // Export selectors engine
 var $$ = Dom7;
 
+if (isAndroid) {
+    // Change class
+    $$('.view .navbar-through').removeClass('navbar-through').addClass('navbar-fixed');
+    // And move Navbar into Page
+    // $$('.view .navbar').prependTo('.view .page');
+}
+
+// Initialize your app
+var myApp = new Framework7({
+    //Enable Material theme for Android device only
+    material: isAndroid ? true : false,
+    //Enable Template7 pages
+    template7pages: true
+});
+
 // Add view
 var mainView = myApp.addView('.view-main', {
+    // Material doesn't support it but don't worry about it
     // Because we use fixed-through navbar we can enable dynamic navbar
     dynamicNavbar: true
 });
+
 /*
 // Callbacks to run specific code for specific pages, for example for About page:
 myApp.onPageInit('asia', function (page) {
@@ -17,62 +41,12 @@ myApp.onPageInit('asia', function (page) {
         createContentPage();
     });
 });
-
-myApp.onPageInit('otherscales', function (page) {
-    // run createContentPage func after link was clicked
-    $$('.create-page').on('click', function () {
-        mainView.router.loadContent('');
-
-    });
-});
-
-myApp.onPageInit('about', function (page) {
-    // run createContentPage func after link was clicked
-    $$('.create-page').on('click', function () {
-        createContentPage();
-    });
-});
-
-myApp.onPageInit('about', function (page) {
-    // run createContentPage func after link was clicked
-    $$('.create-page').on('click', function () {
-        createContentPage();
-    });
-});
-
-myApp.onPageInit('about', function (page) {
-    // run createContentPage func after link was clicked
-    $$('.create-page').on('click', function () {
-        createContentPage();
-    });
-});
 */
 
+/*
 // Generate dynamic page
 var dynamicPageIndex = 0;
 function createContentPage() {
-	mainView.router.loadContent(
-        '<!-- Top Navbar-->' +
-        '<div class="navbar">' +
-        '  <div class="navbar-inner">' +
-        '    <div class="left"><a href="#" class="back link"><i class="icon icon-back"></i><span>Back</span></a></div>' +
-        '    <div class="center sliding">Dynamic Page ' + (++dynamicPageIndex) + '</div>' +
-        '  </div>' +
-        '</div>' +
-        '<div class="pages">' +
-        '  <!-- Page, data-page contains page name-->' +
-        '  <div data-page="dynamic-pages" class="page">' +
-        '    <!-- Scrollable page content-->' +
-        '    <div class="page-content">' +
-        '      <div class="content-block">' +
-        '        <div class="content-block-inner">' +
-        '          <p>Here is a dynamic page created on ' + new Date() + ' !</p>' +
-        '          <p>Go <a href="#" class="back">back</a> or go to <a href="services.html">Services</a>.</p>' +
-        '        </div>' +
-        '      </div>' +
-        '    </div>' +
-        '  </div>' +
-        '</div>'
-    );
+	mainView.router.loadContent(    ''   );
 	return;
-}
+}*/
